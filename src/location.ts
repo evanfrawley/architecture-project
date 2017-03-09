d70c403dfe1c4587cb864cb78f03bf17ae9a2472
Wei-Jen <weijen0330@hotmail.com>
2017-03-09 11:26:56 -0800

    pushed weijen's code

---
+++ src/location.ts
@@ -0,0 +1,18 @@
+/**
+ * Created by Wei-Jen on 1/17/17.
+ */
+require("data/zipcode-locations.json");
+
+class ZcLocation {
+    private zipcode: number;
+
+    constructor(zipcode: number) {
+        this.zipcode = zipcode;
+    }
+
+    getGeolocation(): number[] {
+
+        return
+
+    }
+}
---
+++ src/resistance.js
@@ -0,0 +1,9 @@
+require("data/zipcode-locations.json");
+var ProtestManagementSystem = (function () {
+    function ProtestManagementSystem() {
+        this.protests = [];
+        this.protesters = [];
+        this.movements = [];
+    }
+    return ProtestManagementSystem;
+}());
